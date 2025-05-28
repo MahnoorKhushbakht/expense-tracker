06de730ae986577881ffdecf0d31aaee364d0478
Emmanuel-bot-rgb <pe699674@gmail.com>
2025-05-28 22:37:41 +0500

    updated

--- src/app/expense-tracker/page.js
+++ src/app/expense-tracker/page.js
@@ -1,12 +1,15 @@
 import TagLine from "../components/TagLine";
 import TrackerForm from "../components/TrackerForm";
 import ExpenseTable from "../components/ExpenseTable";
-import DashboardLayout from '../components/DashboardLayout'
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import DashboardLayout from "../components/DashboardLayout";
 
 export default function Expensetracker() {
   return (
-    <DashboardLayout>
-      <div className="flex flex-col max-w-full h-svh flex-grow overflow-auto p-10 bg-white dark:bg-black text-black dark:text-white">
+      <div className="flex flex-col max-w-full h-svh flex-grow overflow-auto bg-white dark:bg-black text-black dark:text-white">
+        <div className="md:hidden lg:hidden sm:flex">
+        <Header/>
         <div className="flex justify-center">
           <TagLine />
         </div>
@@ -14,7 +17,21 @@ export default function Expensetracker() {
         <div className="w-full flex justify-center">
           <ExpenseTable />
       </div>
+      <Footer/>
+      </div>
+        <div className="hidden md:flex lg:flex sm:hidden">
+       <DashboardLayout>
+          <div className="flex justify-center">
+            <TagLine />
+          </div>
+          <TrackerForm />
+          <div className="w-full flex justify-center">
+            <ExpenseTable />
+          </div>
+          <Footer/>
+          </DashboardLayout>
+        </div>
+        
     </div>
-    </DashboardLayout>
   );
 }
